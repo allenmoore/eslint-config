bf565cff436216dc46f315074e8af44227bdcd52
Allen Moore <am@allenmoore.co>
2019-01-22 14:28:52 -0500

    update best practices rules

--- rules/best-practices.js
+++ rules/best-practices.js
@@ -1,107 +1,153 @@
 module.exports = {
-	rules: {
-		'accessor-pairs': 'off',
-		'block-scoped-var': 'error',
-		'complexity': ['off', 11],
-		'class-methods-use-this': 'error',
-		'consistent-return': 'error',
-		'curly': ['error', 'all'],
-		'default-case': ['error', { commentPattern: '^no default$' }],
-		'dot-notation': ['error', { allowKeywords: true }],
-		'dot-location': ['error', 'property'],
-		'eqeqeq': ['error', 'always'],
-		'guard-for-in': 'off',
-		'no-alert': 'error',
-		'no-caller': 'error',
-		'no-case-declarations': 'error',
-		'no-div-regex': 'error',
-		'no-else-return': 'error',
-		'no-empty-function': ['error', {
-			allow: ['arrowFunctions', 'functions', 'methods']
-		}],
-		'no-empty-pattern': 'error',
-		'no-eq-null': 'error',
-		'no-eval': 'error',
-		'no-extend-native': 'error',
-		'no-extra-bind': 'error',
-		'no-extra-label': 'error',
-		'no-fallthrough': 'error',
-		'no-floating-decimal': 'error',
-		'no-global-assign': ['error', {exceptions: []}],
-		'no-implicit-coercion': ['off', {
-			boolean: true,
-			number: true,
-			string: true,
-			allow: []
-		}],
-		'no-implicit-globals': 'off',
-		'no-implied-eval': 'error',
-		'no-invalid-this': 'off',
-		'no-iterator': 'error',
-		'no-labels': ['error', {
-			allowLoop: false,
-			allowSwitch: false
-		}],
-		'no-lone-blocks': 'error',
-		'no-loop-func': 'error',
-		'no-magic-numbers': ['off', {
-			ignore: [],
-			ignoreArrayIndexes: true,
-			enforceConst: true,
-			detectObjects: false,
-		}],
-		'no-multi-str': 'error',
-		'no-native-reassign': 'error',
-		'no-new': 'error',
-		'no-new-func': 'error',
-		'no-new-wrappers': 'error',
-		'no-octal': 'error',
-		'no-octal-escape': 'error',
-		'no-param-reassign': ['error', {
-			props: true
-		}],
-		'no-proto': 'error',
-		'no-redeclare': 'error',
-		'no-restricted-properties': ['error', {
-			object: 'arguments',
-			property: 'callee',
-			message: 'arguments.callee is deprecated',
-		}, {
-			property: '__defineGetter__',
-			message: 'Please use Object.defineProperty instead.',
-		}, {
-			property: '__defineSetter__',
-			message: 'Please use Object.defineProperty instead.',
-		}, {
-			object: 'Math',
-			property: 'pow',
-			message: 'Use the exponentiation operator (**) instead.',
-		}],
-		'no-return-assign': 'error',
-		'no-return-await': 'error',
-		'no-script-url': 'error',
-		'no-self-assign': 'error',
-		'no-self-compare': 'error',
-		'no-sequences': 'error',
-		'no-throw-literal': 'error',
-		'no-unmodified-loop-condition': 'error',
-		'no-unused-expressions': ['error', {
-			allowShortCircuit: false,
-			allowTernary: false
-		}],
-		'no-unused-labels': 'error',
-		'no-useless-call': 'error',
-		'no-useless-concat': 'error',
-		'no-useless-escape': 'error',
-		'no-useless-return': 'error',
-		'no-void': 'error',
-		'no-with': 'error',
-		'radix': 'error',
-		'require-await': 'off',
-		'vars-on-top': 'error',
-		'wrap-iife': ['error', 'any', {
-			functionPrototypeMethods: false
-		}],
-		'yoda': ['error', 'always']
-	}
+  rules: {
+    'accessor-pairs': 'off',
+    'array-callback-return': ['error', { allowImplicit: true }],
+    'block-scoped-var': 'error',
+    'complexity': ['off', 11],
+    'class-methods-use-this': ['error', {
+      exceptMethods: [],
+    }],
+    'consistent-return': 'error',
+    'curly': ['error', 'multi-line'],
+    'default-case': ['error', { commentPattern: '^no default$' }],
+    'dot-notation': ['error', { allowKeywords: true }],
+    'dot-location': ['error', 'property'],
+    'eqeqeq': ['error', 'always', { null: 'ignore' }],
+    'guard-for-in': 'error',
+    'max-classes-per-file': ['off', 1],
+    'no-alert': 'warn',
+    'no-caller': 'error',
+    'no-case-declarations': 'error',
+    'no-div-regex': 'off',
+    'no-else-return': ['error', { allowElseIf: false }],
+    'no-empty-function': ['error', {
+      allow: ['arrowFunctions', 'functions', 'methods']
+    }],
+    'no-empty-pattern': 'error',
+    'no-eq-null': 'off',
+    'no-eval': 'error',
+    'no-extend-native': 'error',
+    'no-extra-bind': 'error',
+    'no-extra-label': 'error',
+    'no-fallthrough': 'error',
+    'no-floating-decimal': 'error',
+    'no-global-assign': ['error', {exceptions: []}],
+    'no-implicit-coercion': ['off', {
+      boolean: true,
+      number: true,
+      string: true,
+      allow: []
+    }],
+    'no-implicit-globals': 'off',
+    'no-implied-eval': 'error',
+    'no-invalid-this': 'off',
+    'no-iterator': 'error',
+    'no-labels': ['error', {
+      allowLoop: false,
+      allowSwitch: false
+    }],
+    'no-lone-blocks': 'error',
+    'no-loop-func': 'error',
+    'no-magic-numbers': ['off', {
+      ignore: [],
+      ignoreArrayIndexes: true,
+      enforceConst: true,
+      detectObjects: false
+    }],
+    'no-multi-spaces': ['error', {
+      ignoreEOLComments: false,
+    }],
+    'no-multi-str': 'error',
+    'no-native-reassign': 'error',
+    'no-new': 'error',
+    'no-new-func': 'error',
+    'no-new-wrappers': 'error',
+    'no-octal': 'error',
+    'no-octal-escape': 'error',
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'acc', // for reduce accumulators
+        'accumulator', // for reduce accumulators
+        'e', // for e.returnvalue
+        'ctx', // for Koa routing
+        'req', // for Express requests
+        'request', // for Express requests
+        'res', // for Express responses
+        'response', // for Express responses
+        '$scope', // for Angular 1 scopes
+      ]
+    }],
+    'no-proto': 'error',
+    'no-redeclare': 'error',
+    'no-restricted-properties': ['error', {
+      object: 'arguments',
+      property: 'callee',
+      message: 'arguments.callee is deprecated',
+    }, {
+      object: 'global',
+      property: 'isFinite',
+      message: 'Please use Number.isFinite instead',
+    }, {
+      object: 'self',
+      property: 'isFinite',
+      message: 'Please use Number.isFinite instead',
+    }, {
+      object: 'window',
+      property: 'isFinite',
+      message: 'Please use Number.isFinite instead',
+    }, {
+      object: 'global',
+      property: 'isNaN',
+      message: 'Please use Number.isNaN instead',
+    }, {
+      object: 'self',
+      property: 'isNaN',
+      message: 'Please use Number.isNaN instead',
+    }, {
+      object: 'window',
+      property: 'isNaN',
+      message: 'Please use Number.isNaN instead',
+    }, {
+      property: '__defineGetter__',
+      message: 'Please use Object.defineProperty instead.',
+    }, {
+      property: '__defineSetter__',
+      message: 'Please use Object.defineProperty instead.',
+    }, {
+      object: 'Math',
+      property: 'pow',
+      message: 'Use the exponentiation operator (**) instead.',
+    }],
+    'no-return-assign': ['error', 'always'],
+    'no-return-await': 'error',
+    'no-script-url': 'error',
+    'no-self-assign': ['error', {
+      props: false,
+    }],
+    'no-self-compare': 'error',
+    'no-sequences': 'error',
+    'no-throw-literal': 'error',
+    'no-unmodified-loop-condition': 'error',
+    'no-unused-expressions': ['error', {
+      allowShortCircuit: false,
+      allowTernary: false,
+      allowTaggedTemplates: false,
+    }],
+    'no-unused-labels': 'error',
+    'no-useless-call': 'error',
+    'no-useless-concat': 'error',
+    'no-useless-catch': 'off',
+    'no-useless-escape': 'error',
+    'no-useless-return': 'error',
+    'no-void': 'error',
+    'no-with': 'error',
+    'prefer-promise-reject-errors': ['error', { allowEmptyReject: true }],
+    'radix': 'error',
+    'require-await': 'off',
+    'require-unicode-regexp': 'off',
+    'vars-on-top': 'error',
+    'wrap-iife': ['error', 'outside', { functionPrototypeMethods: false }],
+    'yoda': 'error'
+  }
 };
